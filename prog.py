15be7983426131d3e66cb200f85cd488e75960cc
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 17:18:09 +0800

    working

--- prog.py
+++ prog.py
@@ -2,6 +2,7 @@ first = 5
 second = 1
 while second <= first:
     for num in range(second):
-        print( num , end='')
+        num = second
+        print(num, end='')
     print()
     second = second + 1
